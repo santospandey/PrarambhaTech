e55a3ca7d2604cc58090e3f5cc1c72f1d7fcf2ca
santosh pandey <santoshpandey@192.168.1.5>
2024-02-26 22:14:13 +0545

    link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,10 +5,11 @@ export default function Header() {
   return (
     <nav className='navbar bg-light mb-4 p-0'>
         <div className="container">
-            <a className="navbar-brand" href="/">
+            <a className="navbar-brand">
                 <div className="d-flex">
                     <img src={logo} alt="logo" className="mr-2" />
-                    <div>Project Management</div>
+                    <button type='button' className="btn btn-secondary" style={{margin: "0px 10px"}}><a href='/' style={{color: "white", textDecoration: "none"}}>Clients</a></button>
+                    <button type='button' className="btn btn-primary"><a href='/projects' style={{color: "white", textDecoration: "none"}}>Projects</a></button>
                 </div>
             </a>
         </div>
